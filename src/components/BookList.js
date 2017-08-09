0d8bbe4cd7a1f3bbf1cc0b8d09b7f398e7d7ac68
Ansoo <asc2683@gmail.com>
2017-08-09 18:45:58 -0400

    refactor: use index as key

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import BookShelf from './BookShelf'
-import { generateKey } from '../helpers'
 
 /*
   use functional stateless component when neither state nor life-cycle method is used
@@ -31,9 +30,9 @@ const BookList = ({header, books, handleBookShelfChange}) => {
       </div>
 
       <div className="list-books-content">
-        {shelves.map(({title, filter, key}) =>
+        {shelves.map(({title, filter, key}, i) =>
           <BookShelf
-            key={generateKey()}
+            key={i}
             title={title}
             books={books.filter((book) => book.shelf === filter)}
             handleBookShelfChange={handleBookShelfChange}
